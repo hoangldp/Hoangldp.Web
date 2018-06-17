c611189ea3977c02e62c6f57fe1b5d882e800672
Hoangldp <danchoisomuaroi@gmail.com>
2018-06-17 16:15:28 +0700

    Add helper

---
+++ Hoangldp.Web/Extenstion/HttpRequestBaseExtension.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Hoangldp.Web.Extenstion
+{
+    public static class HttpRequestBaseExtension
+    {
+        public static bool IsAjax(this HttpRequestBase request)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+            if (request["X-Requested-With"] == "XMLHttpRequest")
+                return true;
+            if (request.Headers != null)
+                return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+            return false;
+        }
+
+        public static string GetIpAddress(this HttpRequestBase request)
+        {
+            var result = string.Empty;
+            if (request.Headers != null)
+            {
+                //The X-Forwarded-For (XFF) HTTP header field is a de facto standard
+                //for identifying the originating IP address of a client
+                //connecting to a web server through an HTTP proxy or load balancer.
+                var forwardedHttpHeader = "X-FORWARDED-FOR";
+                if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["ForwardedHTTPheader"]))
+                {
+                    //but in some cases server use other HTTP header
+                    //in these cases an administrator can specify a custom Forwarded HTTP header
+                    //e.g. CF-Connecting-IP, X-FORWARDED-PROTO, etc
+                    forwardedHttpHeader = ConfigurationManager.AppSettings["ForwardedHTTPheader"];
+                }
+
+                //it's used for identifying the originating IP address of a client connecting to a web server
+                //through an HTTP proxy or load balancer.
+                string xff = request.Headers.AllKeys
+                    .Where(x => forwardedHttpHeader.Equals(x, StringComparison.InvariantCultureIgnoreCase))
+                    .Select(k => request.Headers[k])
+                    .FirstOrDefault();
+
+                //if you want to exclude private IP addresses, then see http://stackoverflow.com/questions/2577496/how-can-i-get-the-clients-ip-address-in-asp-net-mvc
+                if (!String.IsNullOrEmpty(xff))
+                {
+                    string lastIp = xff.Split(new[] { ',' }).FirstOrDefault();
+                    result = lastIp;
+                }
+            }
+
+            if (String.IsNullOrEmpty(result) && request.UserHostAddress != null)
+            {
+                result = request.UserHostAddress;
+            }
+
+            //some validation
+            if (result == "::1")
+                result = "127.0.0.1";
+            //remove port
+            if (!String.IsNullOrEmpty(result))
+            {
+                int index = result.IndexOf(":", StringComparison.InvariantCultureIgnoreCase);
+                if (index > 0)
+                    result = result.Substring(0, index);
+            }
+            return result;
+        }
+    }
+}
